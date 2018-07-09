c0a3a86675740a78d6932dba7c2cae6fce6cfcae
Danny Lu <dannylu8@outlook.com>
2018-07-08 20:56:05 -0700

    install ejs for app view

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-
+/*
 var logger = function(request, response, next){
  console.log('logging...');
  next();
 };
 
 app.use(logger);
+*/
+
+//view Engine (ejs)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+//added json so that will handle json content
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Set Static path for public folder, like css file, jQuery
+//also I created a public folder, that's where I want to put some client stuff inside
+     //commented out for now because I want to play on the backend, not front end yet
+// app.use(express.static(path.join(__dirname,'public')))
+
+
+
 
 app.get('/', function(request, response){
-  response.send('hello world');
+  response.render('index');
 });
 
 
