d59019588537f5994b379c00d9962254f178bd74
Danny Lu <dannylu8@outlook.com>
2018-07-11 10:53:56 -0700

    created, connected to database with MongoDB, orm mongo.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var expressValidator = require('express-validator')
+const { check } = require('express-validator/check');
+
+
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+
 var app = express();
 /*
 var logger = function(request, response, next){
@@ -20,6 +27,18 @@ app.set('views', path.join(__dirname, 'views'))
 //added json so that will handle json content
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+//use expressValidator
+app.use(expressValidator());
+
+//Global vars
+
+app.use(function(request, response, next){
+	//response.locals.something = add global variables
+  response.locals.errors = null;
+  next();
+
+});
 
 //Set Static path for public folder, like css file, jQuery
 //also I created a public folder, that's where I want to put some client stuff inside
@@ -40,25 +59,64 @@ var users = [
 
 ];
 
+app.get('/', function(request, response){
+	db.users.find(function (err, docs) {
+     console.log('docs',docs);
+     response.render('index',{
+  	   title:'customers',
+  	   users:docs
+    });
+  });
+  
+});
+
 app.post('/users/add', function(request, response){
   console.log('FORM Submitted');
+  
 
-  var newUser = {
-  	firstName: request.body.firstName,
-  	lastName: request.body.lastName,
-  	email: request.body.email
-  };
+  request.checkBody('firstName', 'first name is required').notEmpty();
+  request.checkBody('lastName', 'last name is required').notEmpty();
+
+
+  request.checkBody('email', 'email is required').notEmpty();
+  request.checkBody('email', 'email must be an valid e-mail').isEmail();
+
+  var errors = request.validationErrors();
+
+  if(errors){
+    response.render('index', {
+      title:'customers',
+      users:users,
+      errors: errors
+    });
+
+    console.log('ERRORS');
+  }else{
+  	var newUser = {
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email
+  	};
+    db.users.insert(newUser, function(err, result){
+      if(err){
+      	console.log(err);
+      }
+      response.redirect('/');
+    });
+  	console.log('SUCCESS!!!');
+  }
+
+  // var newUser = {
+  // 	firstName: request.body.firstName,
+  // 	lastName: request.body.lastName,
+  // 	email: request.body.email
+  // };
 
   console.log(newUser);
 });
 
 
-app.get('/', function(request, response){
-  response.render('index',{
-  	title:'customers',
-  	users:users
-  });
-});
+
 
 
 app.listen(3000, function(){
